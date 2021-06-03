05815abec17a0fb7dbcc6d5cf1697d84150ba001
Zachary Cytryn <74995726+ZacharyCytryn@users.noreply.github.com>
2021-06-03 10:45:57 -0400

    Add files via upload

---
+++ TodoItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SQLite;
+
+namespace DoToo.Models
+{
+    public class TodoItem
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Workout { get; set; }
+        public bool Completed { get; set; }
+        public DateTime Day { get; set; }
+        public string Notes { get; set; }
+    }
+}
